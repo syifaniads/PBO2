2e1c43c2a036c4ffe7a66b88c72257fc6ad434e6
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-05-05 19:59:41 +0700

    Update Promotion.java

--- Promotion.java
+++ Promotion.java
@@ -5,5 +5,34 @@ abstract class Promotion implements Applicable, Comparable<Promotion> {
     Date startDate;
     Date endDate;
 
-    abstract double calculateDiscount(Order order);
+    @Override
+    public boolean isCustomerEligible(Pelanggan customer) {
+        // Umur akun lebih dari 30 hari
+        if (customer instanceof Guest) {
+            return false; // Guest tidak berlaku
+        } else if (customer instanceof Member) {
+            long membershipDuration = ((Member) customer).getMembershipDuration();
+            return membershipDuration > 30;
+        }
+        return false;
+    }
+    @Override
+    public abstract boolean isMinimumPriceEligible(Order order);
+
+    @Override
+    public abstract boolean isShippingFeeEligible(Order order);
+
+    @Override
+    public abstract double calculateTotalDiscount(Order order) throws Exception;
+
+    @Override
+    public abstract double calculateTotalCashback(Order order) throws Exception;
+
+    @Override
+    public abstract double calculateTotalShippingFeeDiscount(Order order) throws Exception;
+
+    public int compareTo(Promotion o) {
+        // Implementasi perbandingan berdasarkan tanggal mulai
+        return this.startDate.compareTo(o.startDate);
+    }
 }
