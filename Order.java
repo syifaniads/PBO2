46b5c6b3537a81009701885fd887615039117358
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-05-05 17:34:33 +0700

    Update Order.java

--- Order.java
+++ Order.java
@@ -7,20 +7,66 @@ public class Order {
         double discount;
         double total;
         Status status;
+        Promotion promotion;
 
-        void checkOut() {
-            // implementasi checkout
+        public Order(Date orderDate, int orderNumber, double subTotal, double shippingFee, double discount) {
+                this.orderDate = orderDate;
+                this.orderNumber = orderNumber;
+                this.subTotal = subTotal;
+                this.shippingFee = shippingFee;
+                this.discount = discount;
+                this.total = subTotal + shippingFee - discount;
+//                this.vehicle = vehicle;
+//                this.quantity = quantity;
+        }
+        
+        // class Pelanggan harus diisi dari constructor
+        public void checkOut() {
+            if(status == Status.UNPAID){
+                    status = Status.SUCCESSFUL;
+            }
+            else {
+                    System.out.println("Pesanan telah dibayar atau dibatalkan.");
+            }
         }
 
-        void printDetails() {
-            // cetak detail pesanan
+        public void printDetails() {
+                System.out.println("Tanggal Pesanan: " + orderDate);
+                System.out.println("Nomor Pesanan: " + orderNumber);
+                System.out.println("Pesanan:");
+//                System.out.println("   - Kendaraan: " + vehicle.getType());
+//                System.out.println("   - Kuantitas: " + quantity);
+                System.out.println("   - Sub Total: " + subTotal);
+                System.out.println("   - Ongkos Kirim: " + shippingFee);
+                System.out.println("   - Diskon: " + discount);
+                System.out.println("   - Total: " + total);
+                if (promotion != null) {
+//                        System.out.println("Promosi: " + promotion.getPromoCode());
+                }
+                else {
+                        System.out.println("Promosi: Tidak ada");
+                }
         }
 
-        void applyPromo(Promotion promo) {
-            // terapkan promosi
+        public void applyPromo(Promotion promo) {
+                this.promotion = promo;
+                try {
+                        this.discount = promo.calculateDiscount(this);
+                        this.total = subTotal + shippingFee - discount;
+                }
+                catch (Exception e){
+                        System.out.println("Gagal menggunakan promosi.");
+                        e.printStackTrace();
+                }
         }
 
-        void pay() {
-            // simulasi pembayaran
+        public void pay() {
+            if(status == Status.SUCCESSFUL){
+                    System.out.println("Pembayaran berhasil.");
+                    status = Status.PAID;
+            }
+            else {
+                    System.out.println("Pesanan belum dibayar.");
+            }
         }
     }
