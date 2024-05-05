d3267a3e397736ad8769e584b15c8f427ecd64ee
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-05-05 07:15:59 +0700

    Create Order.java

---
+++ Order.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Order {
+        Date orderDate;
+        int orderNumber;
+        double subTotal;
+        double shippingFee;
+        double discount;
+        double total;
+        Status status;
+
+        void checkOut() {
+            // implementasi checkout
+        }
+
+        void printDetails() {
+            // cetak detail pesanan
+        }
+
+        void applyPromo(Promotion promo) {
+            // terapkan promosi
+        }
+
+        void pay() {
+            // simulasi pembayaran
+        }
+    }
