71ae2026f862b3e058a8b73e0328f14b7b2d901e
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-05-05 07:19:01 +0700

    Create CashbackPromo.java

---
+++ CashbackPromo.java
@@ -0,0 +1,42 @@
+class CashbackPromo extends Promotion {
+    @Override
+    double calculateDiscount(Order order) {
+        // hitung cashback
+        return 0;
+    }
+
+    @Override
+    public int compareTo(Promotion o) {
+        return 0;
+    }
+
+    @Override
+    public boolean isCustomerEligible(Pelanggan customer) {
+        return false;
+    }
+
+    @Override
+    public boolean isMinimumPriceEligible(Order order) {
+        return false;
+    }
+
+    @Override
+    public boolean isShippingFeeEligible(Order order) {
+        return false;
+    }
+
+    @Override
+    public double calculateTotalDiscount(Order order) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public double calculateTotalCashback(Order order) throws Exception {
+        return 0;
+    }
+
+    @Override
+    public double calculateTotalShippingFeeDiscount(Order order) throws Exception {
+        return 0;
+    }
+}
