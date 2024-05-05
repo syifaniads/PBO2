bbe7444f6be470f6dd13baa9a3e2c2ace27df7ba
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-05-05 17:50:58 +0700

    Update Member.java

--- Member.java
+++ Member.java
@@ -1,16 +1,31 @@
 import java.util.*;
 
 class Member extends Pelanggan {
-    Date membershipDate;
+    private Date membershipDate;
 
+    public Member(String firstName, String lastName, Date membershipDate) {
+        super(firstName, lastName);
+        this.membershipDate = membershipDate;
+    }
     @Override
     String getFullName() {
         return firstName + " " + lastName;
     }
 
-    long getMembershipDuration() {
-        // hitung durasi keanggotaan
-        return 0;
+    @Override
+    Order makeOrder(Date orderDate, int orderNumber, double subTotal, double shippingFee, double discount) {
+        return new Order(orderDate, orderNumber, subTotal, shippingFee, discount);
+    }
+
+    @Override
+    void confirmPay(int orderNumber) {
+        System.out.println("Konfirmasi pembayaran pesanan " + orderNumber + " oleh member " + getFullName());
+    }
+
+    public long getMembershipDuration() {
+        Date today = new Date();
+        long diffInMillies = Math.abs(today.getTime() - membershipDate.getTime());
+        return diffInMillies / (1000 * 60 * 60 * 24); // Konversi dari milisecond ke hari
     }
 
 }
