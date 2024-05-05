07b30190e351202586b4ce95ae6110082d6f23e7
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-05-05 07:19:50 +0700

    Create Applicable.java

---
+++ Applicable.java
@@ -0,0 +1,10 @@
+interface Applicable {
+    boolean isCustomerEligible(Pelanggan customer);
+    boolean isMinimumPriceEligible(Order order);
+    boolean isShippingFeeEligible(Order order);
+    double calculateTotalDiscount(Order order) throws Exception;
+
+    double calculateTotalCashback(Order order) throws Exception;
+
+    double calculateTotalShippingFeeDiscount(Order order) throws Exception;
+}
