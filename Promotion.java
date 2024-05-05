3f3d8a96bf198921f93e15355fc97525f865fdcf
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-05-05 07:16:41 +0700

    Create Promotion.java

---
+++ Promotion.java
@@ -0,0 +1,7 @@
+abstract class Promotion implements Applicable, Comparable<Promotion> {
+    String promoCode;
+    Date startDate;
+    Date endDate;
+
+    abstract double calculateDiscount(Order order);
+}
