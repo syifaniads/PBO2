965c9f4696c3acbb9183d0dc086579aa0b611e06
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-05-05 17:51:19 +0700

    Update Guest.java

--- Guest.java
+++ Guest.java
@@ -1,6 +1,22 @@
+import java.util.Date;
+
 class Guest extends Pelanggan {
+
+    public Guest(String firstName, String lastName) {
+        super(firstName, lastName);
+    }
     @Override
     String getFullName() {
         return firstName + " " + lastName;
+    }
+
+    @Override
+    Order makeOrder(Date orderDate, int orderNumber, double subTotal, double shippingFee, double discount) {
+        return new Order(orderDate, orderNumber, subTotal, shippingFee, discount);
+    }
+
+    @Override
+    void confirmPay(int orderNumber) {
+        System.out.println("Konfirmasi pembayaran pesanan " + orderNumber + " sebagai guest.");
     }
 }
