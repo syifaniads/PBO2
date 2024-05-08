a4cfd435096b397769d46bd5d73c7efad256c224
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-05-08 23:34:13 +0700

    Costumer.java

--- Pelanggan.java
+++ Pelanggan.java
@@ -1,17 +1,17 @@
 import java.util.*;
-public abstract class Pelanggan {
+public abstract class Costumer {
     protected String firstName;
     protected String lastName;
 //    protected String address;
 //    protected String phoneNumber;
 
-    public Pelanggan(String firstName, String lastName) {
+    public Costumer(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
     }
-    abstract String getFullName();
+    public abstract String getFullName();
 
-    abstract Order makeOrder(Date orderDate, int orderNumber, double subTotal, double shippingFee, double discount);
+    abstract Order makeOrder(Date orderDate, double subTotal, double shippingFee, double discount);
 
     abstract void confirmPay(int orderNumber);
 }
