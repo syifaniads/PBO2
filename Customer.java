3838b3a484aab3d13f4a40e70f7fda4ad91311f8
latifaanggia <162555167+latifaanggia@users.noreply.github.com>
2024-05-27 07:55:37 +0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,14 +1,16 @@
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public abstract class Customer {
+    protected String id;
     protected String firstName;
     protected String lastName;
     protected int balance;
-    protected Map<Menu, Integer> cart;
+    protected Map<Menu, CartItem> cart;
 
-    public Customer(String firstName, String lastName, int initialBalance) {
+    public Customer(String id, String firstName, String lastName, int initialBalance) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.balance = initialBalance;
@@ -20,6 +22,31 @@ public abstract class Customer {
     public abstract boolean addToCart(Menu menuItem, int qty, String startDate);
     public abstract boolean removeFromCart(Menu menuItem, int qty, String startDate);
     abstract Order makeOrder(LocalDate orderDate, LocalDate endDate, double subTotal, double shippingFee, double discount, double total);
-    public abstract Map<Menu, Integer> getCart();
+    public abstract Map<Menu, CartItem> getCart();
     abstract void confirmPay(int orderNumber);
+    public void printOrder() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        System.out.println("Kode Pemesan: " + id);
+        System.out.println("Nama: " + getFullName());
+
+        List<CartItem> menusToPrint = new ArrayList<>(cart.values());
+
+        menusToPrint.sort(Comparator.comparing((CartItem c) -> LocalDate.parse(c.startDate, formatter)).thenComparingDouble(c -> c.menuItem.Harga));
+
+        System.out.println("No | Menu                                 |  Qty  | Subtotal      ");
+        System.out.println("=================================");
+        int no = 1;
+        double subtotal = 0;
+        for (CartItem cartItem : menusToPrint) {
+            double itemSubtotal = cartItem.qty * cartItem.menuItem.Harga;
+            System.out.printf("%2d  | %-30s | %4d  | %.3f      \n", no++, cartItem.menuItem.NamaMenu + " " + cartItem.menuItem.PlatNomor, cartItem.qty, itemSubtotal);
+            System.out.printf("       %s\n", cartItem.startDate);
+            subtotal += itemSubtotal;
+        }
+        System.out.println("=================================");
+        System.out.printf("SubTotal                                    :            %.3f\n", subtotal);
+        System.out.println("=================================");
+        System.out.printf("Total                                          :            %.3f\n", subtotal);
+        System.out.printf("Saldo                                         :            %.3f\n", (double)balance);
+    }
 }
